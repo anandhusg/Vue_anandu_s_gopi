f7bd13f718188c5d383461d5e9f9015b9f5966eb
AnandhuSG098 <anandhusg260198@gmail.com>
2025-06-14 11:58:44 +0530

    commit

--- space_objects/src/views/HomeView.vue
+++ space_objects/src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
+<h1>
+  hello friends
+
+</h1>
+  <!-- <main>
     <TheWelcome />
-  </main>
+  </main> -->
 </template>
